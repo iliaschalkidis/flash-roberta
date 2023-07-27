6c48d9f1d9a7ae1212963f9346d4b5255ab31d62
kiddothe2b <ilias.chalkidis@di.ku.dk>
2023-07-27 11:07:32 +0200

    init commit

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=roberta-flash-attention
+#SBATCH --cpus-per-task=8 --mem=8000M
+#SBATCH --exclude=hendrixgpu01fl
+#SBATCH -p gpu --gres=gpu:a100:1
+#SBATCH --output=/home/rwg642/flash-roberta/roberta-flash-attention.txt
+#SBATCH --time=0:20:00
+
+module load miniconda/4.12.0
+conda activate kiddothe2b
+
+echo $SLURMD_NODENAME
+echo $CUDA_VISIBLE_DEVICES
+
+python demo_flashroberta.py --model_class roberta
+python demo_flashroberta.py --model_class flashroberta
