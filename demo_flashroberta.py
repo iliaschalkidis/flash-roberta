14403e1daca8e013ee7a9c905665679f34651774
kiddothe2b <ilias.chalkidis@di.ku.dk>
2023-07-27 13:01:33 +0200

    init commit

--- demo_flashroberta.py
+++ demo_flashroberta.py
@@ -3,9 +3,13 @@ from modeling_flashroberta import FlashRobertaForMaskedLM
 import torch
 from torch.cuda.amp import autocast
 import argparse
+from datasets import load_dataset
+import time
+import re
+import random
 
 
-def demo_mlm(model_class: str = 'roberta'):
+def demo_mlm(corpus, model_class: str = 'roberta', log_predictions: bool = False):
 
     # Check if you have a GPU with CUDA support
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -17,28 +21,49 @@ def demo_mlm(model_class: str = 'roberta'):
     elif model_class == 'flashroberta':
         model = FlashRobertaForMaskedLM.from_pretrained('roberta-base').to(device)
 
-    # Prepare the input sequence
-    sequence = f"My favourite pet is a {tokenizer.mask_token}."
-    print('Example sequence:', sequence)
-    input = tokenizer.encode(sequence, return_tensors="pt").to(device)
+    current_time = time.time()
+    correct_predictions = 0
+    for document in corpus:
 
-    # Ensure mask_token_id is of type torch.float16
-    mask_token_id = torch.tensor(tokenizer.mask_token_id).to(device)
+        input = tokenizer.encode(document['text'], return_tensors="pt").to(device)
 
-    # Generate the masked language model output
-    with autocast():
-        mask_token_index = torch.where(input == mask_token_id)[1]
+        # Ensure mask_token_id is of type torch.float16
+        mask_token_id = torch.tensor(tokenizer.mask_token_id).to(device)
 
-        token_logits = model(input)[0]
+        # Generate the masked language model output
+        with autocast():
+            mask_token_index = torch.where(input == mask_token_id)[1]
 
-        mask_token_logits = token_logits[0, mask_token_index, :]
-        top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+            token_logits = model(input)[0]
+            mask_token_logits = token_logits[0, mask_token_index, :]
+            top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+            correct_predictions += int(document['masked_word'] in tokenizer.decode(top_5_tokens))
 
-        print(f'Top 5 tokens for model: {model.__class__.__name__}')
-        print('-' * 50)
-        for top_k, token in enumerate(top_5_tokens):
-            print('Top {} token: {}'.format(top_k, tokenizer.decode([token])))
-        print('-' * 50)
+            if log_predictions:
+                print(f'Top 5 tokens for model: {model.__class__.__name__}')
+                print('-' * 50)
+                for top_k, token in enumerate(top_5_tokens):
+                    print('Top {} token: {}'.format(top_k, tokenizer.decode([token])))
+                print('-' * 50)
+
+    end_time = time.time() - current_time
+    print(f'Model: {model.__class__.__name__}')
+    print(f'Time taken for {model.__class__.__name__}: {end_time}')
+    print(f'Recall@5 for {model.__class__.__name__}: {correct_predictions / len(corpus)}')
+
+
+def mask_random_words(document):
+    # Tokenize the text into words using regular expression
+    words = re.findall(r'\b\w+\b', document['text'])
+
+    # Randomly select a word to mask
+    word_to_mask = random.choice(words)
+
+    # Replace the selected word with the mask token
+    document['text'] = document['text'].replace(word_to_mask, "<mask>", 1)
+    document['masked_word'] = word_to_mask
+
+    return document
 
 
 if __name__ == "__main__":
@@ -47,4 +72,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    demo_mlm(args.model_class)
+    # Load C4 dataset, take 1000 samples, and mask random words
+    c4_dataset = load_dataset("c4", "en", split="train", streaming=True)
+    c4_subset = c4_dataset.take(1000)
+    c4_subset = c4_subset.map(mask_random_words)
+
+    demo_mlm(c4_subset, args.model_class)
