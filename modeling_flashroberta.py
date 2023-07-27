ff84933901adb5e3f5a3009d623d93b218bb9117
kiddothe2b <ilias.chalkidis@di.ku.dk>
2023-07-27 12:08:12 +0200

    init commit

--- modeling_flashroberta.py
+++ modeling_flashroberta.py
@@ -50,102 +50,24 @@ class FlashRobertaSelfAttention(nn.Module):
         past_key_value: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
         output_attentions: Optional[bool] = False,
     ) -> Tuple[torch.Tensor]:
-        mixed_query_layer = self.query(hidden_states)
 
-        # If this is instantiated as a cross-attention module, the keys
-        # and values come from an encoder; the attention mask needs to be
-        # such that the encoder's padding tokens are not attended to.
-        is_cross_attention = encoder_hidden_states is not None
-
-        if is_cross_attention and past_key_value is not None:
-            # reuse k,v, cross_attentions
-            key_layer = past_key_value[0]
-            value_layer = past_key_value[1]
-            attention_mask = encoder_attention_mask
-        elif is_cross_attention:
-            key_layer = self.transpose_for_scores(self.key(encoder_hidden_states))
-            value_layer = self.transpose_for_scores(self.value(encoder_hidden_states))
-            attention_mask = encoder_attention_mask
-        elif past_key_value is not None:
-            key_layer = self.transpose_for_scores(self.key(hidden_states))
-            value_layer = self.transpose_for_scores(self.value(hidden_states))
-            key_layer = torch.cat([past_key_value[0], key_layer], dim=1)
-            value_layer = torch.cat([past_key_value[1], value_layer], dim=1)
-        else:
-            key_layer = self.transpose_for_scores(self.key(hidden_states))
-            value_layer = self.transpose_for_scores(self.value(hidden_states))
-
-        query_layer = self.transpose_for_scores(mixed_query_layer)
-        # attention_mask = attention_mask.view(-1, attention_mask.size(-1), 1, 1)
-
-        use_cache = past_key_value is not None
-        if self.is_decoder:
-            # if cross_attention save Tuple(torch.Tensor, torch.Tensor) of all cross attention key/value_states.
-            # Further calls to cross_attention layer can then reuse all cross-attention
-            # key/value_states (first "if" case)
-            # if uni-directional self-attention (decoder) save Tuple(torch.Tensor, torch.Tensor) of
-            # all previous decoder key/value_states. Further calls to uni-directional self-attention
-            # can concat previous decoder key/value_states to current projected key/value_states (third "elif" case)
-            # if encoder bi-directional self-attention `past_key_value` is always `None`
-            past_key_value = (key_layer, value_layer)
+        key_layer = self.transpose_for_scores(self.key(hidden_states))
+        value_layer = self.transpose_for_scores(self.value(hidden_states))
+        query_layer = self.transpose_for_scores(self.query(hidden_states))
 
         # Flash Attention
-        context_layer = flash_attn_func(query_layer, key_layer, value_layer, dropout_p=self.dropout_rate,
-                                        softmax_scale=None, causal=self.is_decoder)
+        context_layer, _, attention_probs = flash_attn_func(query_layer, key_layer, value_layer,
+                                                            dropout_p=self.dropout_rate,
+                                                            softmax_scale=None,
+                                                            causal=self.is_decoder,
+                                                            return_attn_probs=True)
 
-        # print(attention_scores.size())
-        #
-        # if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
-        #     query_length, key_length = query_layer.shape[2], key_layer.shape[2]
-        #     if use_cache:
-        #         position_ids_l = torch.tensor(key_length - 1, dtype=torch.long, device=hidden_states.device).view(
-        #             -1, 1
-        #         )
-        #     else:
-        #         position_ids_l = torch.arange(query_length, dtype=torch.long, device=hidden_states.device).view(-1, 1)
-        #     position_ids_r = torch.arange(key_length, dtype=torch.long, device=hidden_states.device).view(1, -1)
-        #     distance = position_ids_l - position_ids_r
-        #
-        #     positional_embedding = self.distance_embedding(distance + self.max_position_embeddings - 1)
-        #     positional_embedding = positional_embedding.to(dtype=query_layer.dtype)  # fp16 compatibility
-        #
-        #     if self.position_embedding_type == "relative_key":
-        #         relative_position_scores = torch.einsum("bhld,lrd->bhlr", query_layer, positional_embedding)
-        #         attention_scores = attention_scores + relative_position_scores
-        #     elif self.position_embedding_type == "relative_key_query":
-        #         relative_position_scores_query = torch.einsum("bhld,lrd->bhlr", query_layer, positional_embedding)
-        #         relative_position_scores_key = torch.einsum("bhrd,lrd->bhlr", key_layer, positional_embedding)
-        #         attention_scores = attention_scores + relative_position_scores_query + relative_position_scores_key
-        #
-        # attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-        #
-        # if attention_mask is not None:
-        #     # Apply the attention mask is (precomputed for all layers in FlashRobertaModel forward() function)
-        #     attention_scores = attention_scores + attention_mask
-        #
-        # # Normalize the attention scores to probabilities.
-        # attention_probs = nn.functional.softmax(attention_scores, dim=-1)
-        #
-        # # This is actually dropping out entire tokens to attend to, which might
-        # # seem a bit unusual, but is taken from the original Transformer paper.
-        # attention_probs = self.dropout(attention_probs)
-        #
-        # # Mask heads if we want to
-        # if head_mask is not None:
-        #     attention_probs = attention_probs * head_mask
-        #
-        # print(attention_probs.size(), value_layer.size())
-        #
-        # context_layer = torch.matmul(attention_probs, value_layer)
-        #
-        # context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        # Merge heads
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
         context_layer = context_layer.view(new_context_layer_shape)
 
-        outputs = (context_layer, None) if output_attentions else (context_layer,)
+        outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
 
-        if self.is_decoder:
-            outputs = outputs + (past_key_value,)
         return outputs
 
 
@@ -174,9 +96,6 @@ class FlashRobertaModel(RobertaModel):
         # Replace legacy RobertaSelfAttention with FlashRobertaSelfAttention
         for attention_layer in self.encoder.layer:
             attention_layer.attention.self = FlashRobertaSelfAttention(config)
-
-        for idx, attention_layer in enumerate(self.encoder.layer):
-            print(f'{idx}: {attention_layer.attention.self.__class__.__name__}')
 
         # Initialize weights and apply final processing
         self.post_init()
